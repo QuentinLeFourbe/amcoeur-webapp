b4ad0cd87af7180fd39f4d5152069c59c4d8808e
Quentin GARCIA <quentingarcia40@gmail.com>
2023-09-28 22:15:08 +0200

    fix black background

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,6 +5,9 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
   plugins: [
     { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
     react(),
