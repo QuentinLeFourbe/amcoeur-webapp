915d37a4ea3895dbac2e3ee75436b6016a7bf018
Quentin GARCIA <quentingarcia40@gmail.com>
2023-09-28 21:37:12 +0200

    feat: :sparkles: Basis for email management

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
 import path from "path";
 import helmet from "helmet";
+import emailRoutes from "./routes/email";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "../client/dist")));
-// app.use(helmet());
+
+app.use("/api/email", emailRoutes);
 
 app.get("/*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
