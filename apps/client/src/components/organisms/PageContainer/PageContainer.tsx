6a0ca3c39f871bfc656bc4471eed4bab91e63396
Quentin GARCIA <quentingarcia40@gmail.com>
2023-10-01 22:56:42 +0200

    fade in un peu pérave

--- apps/client/src/components/organisms/PageContainer/PageContainer.tsx
+++ apps/client/src/components/organisms/PageContainer/PageContainer.tsx
@@ -6,15 +6,26 @@ import { useEffect, useState } from "react";
 
 function PageContainer() {
   const { pathname } = useLocation();
+  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  //show content after 1s
+  useEffect(() => {
+    setShowContent(false);
+    setTimeout(() => {
+      setShowContent(true);
+    }, 100);
+  }, [pathname]);
+
   return (
     <div className={container}>
       <Header />
-      <Outlet />
+      <div className={showContent ? fadeIn : hidden}>
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
@@ -25,6 +36,15 @@ const container = css({
   flexFlow: "column nowrap",
   alignItems: "stretch",
   minHeight: "100vh",
+});
+
+const hidden = css({
+  opacity: 0,
+});
+
+const fadeIn = css({
+  opacity: 1,
+  transition: "opacity 0.5s ease-in-out ",
 });
 
 export default PageContainer;
