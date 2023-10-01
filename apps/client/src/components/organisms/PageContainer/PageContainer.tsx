d933dbc51e917a55f100b5604d56a84cea743c94
Quentin GARCIA <quentingarcia40@gmail.com>
2023-10-01 22:51:43 +0200

    remove ppor fade in effect

--- apps/client/src/components/organisms/PageContainer/PageContainer.tsx
+++ apps/client/src/components/organisms/PageContainer/PageContainer.tsx
@@ -6,25 +6,15 @@ import { useEffect, useState } from "react";
 
 function PageContainer() {
   const { pathname } = useLocation();
-  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  //show content after 1s
-  useEffect(() => {
-    setTimeout(() => {
-      setShowContent(true);
-    }, 1000);
-  }, []);
-
   return (
     <div className={container}>
       <Header />
-      <div className={showContent ? fadeIn : hidden}>
-        <Outlet />
-      </div>
+      <Outlet />
       <Footer />
     </div>
   );
@@ -35,15 +25,6 @@ const container = css({
   flexFlow: "column nowrap",
   alignItems: "stretch",
   minHeight: "100vh",
-});
-
-const hidden = css({
-  opacity: 0,
-});
-
-const fadeIn = css({
-  opacity: 1,
-  transition: "opacity 0.5s ease-in-out ",
 });
 
 export default PageContainer;
