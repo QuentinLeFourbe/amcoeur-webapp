603ac141b8691816077df8ebe76f464ccdfd7c5e
Quentin GARCIA <quentingarcia40@gmail.com>
2023-12-13 15:12:26 +0100

    Mongodb setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,9 +4,12 @@ import path from "path";
 import helmet from "helmet";
 import emailRoutes from "./routes/email";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const uri = "mongodb://localhost:27017/amcoeur";
+
 app.use(express.json());
 
 app.use(bodyParser.json());
@@ -19,16 +22,24 @@ app.use(
         "frame-src": ["'self'", "www.google.com", "www.gstatic.com"],
       },
     },
-  })
+  }),
 );
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 app.use("/api/email", emailRoutes);
+// add page route here
 
 app.get("/*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connexion à la base de données établie"))
+  .catch((err) =>
+    console.error("Erreur de connexion à la base de données:", err),
+  );
 
 app.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
