8b4acc9721ab966fb095a9837a0bf10707fde563
Anthony Lines <linesanthony018@gmail.com>
2023-12-15 14:41:04 +0100

    getAllPages working

--- apps/server/src/routes/page.ts
+++ apps/server/src/routes/page.ts
@@ -3,6 +3,7 @@ import { createPage, getAllPages } from "../controllers/page";
 
 const router = Router();
 
-router.post("/", createPage, getAllPages);
+router.post("/", createPage);
+router.get("/", getAllPages);
 // / car si autre route on peut les différencié alors que avec /*
 export default router;
