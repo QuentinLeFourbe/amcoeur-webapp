bf9ef08dddc2b508ffae05d943a140e1cb056039
Quentin GARCIA <quentingarcia40@gmail.com>
2024-01-17 11:13:57 +0100

    remove commented code

--- apps/server/src/models/page.ts
+++ apps/server/src/models/page.ts
@@ -1,11 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 
-// const page = new Schema({
-//   title: String,
-//   route: String,
-//   content: String,
-// });
-
 const page = new Schema(
   {
     name: { type: String, required: true },
