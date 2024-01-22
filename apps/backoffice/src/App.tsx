72f32be8cf729933c2038fff0c04326b51b648d6
Quentin GARCIA <quentingarcia40@gmail.com>
2024-01-22 15:46:00 +0100

    fix build

--- apps/backoffice/src/App.tsx
+++ apps/backoffice/src/App.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import PageContainer from "./components/template/PageContainer/PageContainer";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
