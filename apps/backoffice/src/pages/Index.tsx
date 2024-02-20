4b8b212e49284c58eba043930d0536d7c9b77d16
Quentin Garcia <quentingarcia40@gmail.com>
2024-02-20 11:41:10 +0100

    Add confirmation message for delete

--- apps/backoffice/src/pages/Index.tsx
+++ apps/backoffice/src/pages/Index.tsx
@@ -11,6 +11,12 @@ function Index() {
     <p>
       Si vous rencontrez des difficultés, ou que vous avez des suggestions pour l&apos;améliorer, veuillez envoyer un mail à: quentingarcia40@gmail.com
     </p>
+    <h2>Comment fonctionne les pages</h2>
+    <p>
+      Les pages sont écrites au format Markdown. Ce format permet de créer une page sans se soucier de l&apos;aspect graphique de la page.
+      Vous n&apos;avez pas la main sur le rendu graphique de la page, pour toute modification ou suggestion, merci de m&apos;envoyer un mail.
+      Toutes les fonctionnalités ne sont pas encore présentes.
+    </p>
   </div>;
 }
 
