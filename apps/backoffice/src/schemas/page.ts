8473fd72608bd02a597f8a790b3262db4c9b470e
Quentin Garcia <quentingarcia40@gmail.com>
2024-02-20 11:54:17 +0100

    Add schema validation for route

--- apps/backoffice/src/schemas/page.ts
+++ apps/backoffice/src/schemas/page.ts
@@ -3,9 +3,9 @@ import { PageData } from "@amcoeur/types";
 import { Types } from "mongoose";
 
 export const pageDataSchema: yup.ObjectSchema<PageData> = yup.object().shape({
-  name: yup.string().required("Veuiilez renseigner le nom de la page"),
+  name: yup.string().required("Veuillez renseigner le nom de la page"),
   route: yup
-    .string()
+    .string().lowercase().matches(/^[a-z0-9-]+$/, "Le chemin d'accès ne doit contenir que des lettres minuscules, chiffres ou tirets.")
     .required("Veuiilez renseigner le chemin d'accès de la page"),
   components: yup.array().required("Veuillez ajouter des composants à la page"),
   _id: yup.mixed<Types.ObjectId>(),
