bed5a651c53cfefe60662be337435bd844631b90
Quentin Garcia <quentingarcia40@gmail.com>
2024-09-10 16:36:47 +0200

    add cross origin ressource policy

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -40,6 +40,10 @@ const corsOptions =
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     };
 
+app.use((_req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  next();
+});
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
