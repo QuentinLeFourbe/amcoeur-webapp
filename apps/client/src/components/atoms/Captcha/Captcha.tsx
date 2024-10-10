0b0703c2e8bba3428fdd3a509ae31bcc24318921
Quentin Garcia <quentingarcia40@gmail.com>
2024-10-10 18:34:42 +0200

    Fix recaptcha for csp

---
+++ apps/client/src/components/atoms/Captcha/Captcha.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import ErrorLabel from "../ErrorLabel/ErrorLabel";
+
+type CaptchaProps = {
+  errorMessage?: string;
+  setFormValue: (value: string) => void;
+};
+
+const Captcha = forwardRef<ReCAPTCHA | null, CaptchaProps>(function Captcha(
+  { errorMessage, setFormValue }: CaptchaProps,
+  ref,
+) {
+  const handleCaptchaChange = (value: string | null) => {
+    setFormValue(value || "");
+  };
+  return (
+    <div>
+      <ReCAPTCHA
+        sitekey="6Le4vnUoAAAAAKCNMteCbEZ6Wd7S5lG8SIgwocGM"
+        ref={ref}
+        onChange={handleCaptchaChange}
+      />
+      {errorMessage && <ErrorLabel>{errorMessage}</ErrorLabel>}
+    </div>
+  );
+});
+
+export default Captcha;
