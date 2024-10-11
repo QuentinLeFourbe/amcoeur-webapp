8ce068ffaa40975b0ad3b1a3c5fbc95ffa4d9dc2
Quentin Garcia <quentingarcia40@gmail.com>
2024-10-11 17:55:16 +0200

    Remove console log

--- apps/client/src/components/organisms/DynamicFormRenderer/DynamicFormRenderer.tsx
+++ apps/client/src/components/organisms/DynamicFormRenderer/DynamicFormRenderer.tsx
@@ -97,7 +97,6 @@ const DynamicForm = ({
   const recaptchaRef = useRef<ReCAPTCHA>(null);
 
   const submitData = (data: FormAnswersClient) => {
-    console.log({ data });
     onSubmit(data);
     reset();
     recaptchaRef?.current?.reset();
