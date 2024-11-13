96093167f7b3f7b285239936ce42a7976e7d8f80
Quentin Garcia <quentingarcia40@gmail.com>
2024-11-13 11:52:36 +0100

    auto login redirect to the previous user page

--- apps/backoffice/src/global/hooks/useUser.tsx
+++ apps/backoffice/src/global/hooks/useUser.tsx
@@ -1,23 +1,42 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
 import { getCurrentUser } from "../api/users";
-import router from "../../routes";
 import { checkUserPermissions } from "../utils/user";
 import { UserContext } from "../contexts/user";
+import { microsoftLogin } from "../utils/auth";
 
 export const useCurrentUser = () => {
+  const { accessToken, setAccessToken } = useContext(UserContext) || {};
+  const navigate = useNavigate();
+
   const query = useQuery({
     queryKey: ["currentUser"],
     queryFn: getCurrentUser,
     retry: 0,
-    onError: () => {
-      router.navigate("/login", { replace: true });
-    },
     onSuccess: (data) => {
       if (checkUserPermissions(data.data, ["inactive"]))
-        router.navigate("/inactive", { replace: true });
+        navigate("/inactive", { replace: true });
+    },
+    onError: (error: AxiosError) => {
+      if (error.status === 401 && accessToken) {
+        setAccessToken?.("");
+      }
     },
   });
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!accessToken) {
+      if (isLoggedIn) {
+        microsoftLogin();
+      } else if (window.location.pathname !== "/login") {
+        navigate("/login", { replace: true });
+      }
+    }
+  }, [accessToken]);
+
   return { currentUser: query.data?.data, ...query };
 };
 
